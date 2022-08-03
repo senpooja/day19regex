138650330809d499637794b99032ac15fe19d824
senpooja <poojasen744055@gmail.com>
2022-08-03 18:16:42 +0530

    wellcome uc-7 file

---
+++ uc-7regex/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace day19assignment
+{
+
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Patterns patterns = new Patterns();
+            //Console.WriteLine(patterns.validatePinCode("xyz@bridgelabz.com.in.in.in"));
+            if (patterns.validateString("Akashs14"))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+                Console.WriteLine("Invalid");
+            Console.ReadKey();
+        }
+    }
+}
