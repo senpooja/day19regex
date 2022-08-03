f9a9e17c64f3d9624aea8dea56a207a26bfd340e
senpooja <poojasen744055@gmail.com>
2022-08-03 18:24:40 +0530

    wellcome uc-9 file

---
+++ uc-9emailvalidation/checkmail.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace uc_9emailvalidation
+{
+    internal class ninthemail
+    {
+        public static string Regex_Pincode = "^[a-zA-Z0-9]+([.#_$+-][a-zA-Z0-9]*)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2})?$";
+        
+        public bool validateString(string sample)
+        {
+            return Regex.IsMatch(sample, Regex_Pincode);
+        }
+    }
+}
